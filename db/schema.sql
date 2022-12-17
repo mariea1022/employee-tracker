f627b02c569924b68f72e30bee368e60f20bf535
Marie Lewis <mariea1022@gmail.com>
2022-12-17 12:54:36 -0800

    established connection to the databse; entered 1 query to view all departments

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,13 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     primary key (id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const mysql = require("mysql2");
 
 const mainMenuQuestions = [
     {
@@ -13,21 +14,55 @@ const mainMenuQuestions = [
 
 const departmentQuestions = [
     {
-
+        type: "input",
+        message: "What is the name of the department?",
+        name: departmenName
     }
-]
+];
 
 const roleQuestions = [
     {
-
+        type: "input",
+        message: "What is the name of the role?",
+        name: roleName
+    },
+    {
+        type: "number",
+        message: "What is the salary of the role?",
+        name: roleSalary
+    },
+    {
+        type: "list",
+        message: "Which department does this role belong to?",
+        choices: [],
+        name: roleDepartment
     }
-]
+];
 
 const employeeQuestions = [
     {
-
+        type: "input",
+        message: "What is the employee's first name?",
+        name: employeeFirstName
+    },
+    {
+        type: "input",
+        message: "What is the employee's last name?",
+        name: employeeLastName
+    },
+    {
+        type: "list",
+        message: "What is the employee's role?",
+        choices: [],
+        name: employeeRole
+    },
+    {
+        type: "list",
+        message: "Who is the employee's manager?",
+        choices: [],
+        name: employeeManager
     }
-]
+];
 
 // function to initialize app
 function init() {
@@ -42,3 +77,24 @@ function mainQuestions() {
 }
 
 init();
+
+// to connect to the database
+const dbConnection = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "employee_db",
+    },
+    console.log(`Connected to the employee_db database.`)
+);
+
+// querries to the database using the dbConnection
+// should have a query to view all departments, all roles, all employees?
+dbConnection.query("SELECT * FROM department", function (err, results) {
+    console.log(results);
+
+    // when inquirer questions are done, end the connection
+    dbConnection.end();
+});
+
